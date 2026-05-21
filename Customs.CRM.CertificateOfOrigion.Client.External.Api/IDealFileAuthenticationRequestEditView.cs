b3a00f126e29c5c5f99cf0f2692f8980e3104bfd
GIV\kobi_dv <Kobi_dv@malam.com>
2026-05-21 15:06:53 +0300

    commit

--- Customs.CRM.CertificateOfOrigion.Client.External.Api/IDealFileAuthenticationRequestEditView.cs
+++
@@ -1,9 +0,0 @@
-﻿using Customs.Inf.MMI.Common.CAL;
-
-
-namespace Customs.CRM.CertificateOfOrigion.Client.External.Api
-{
-    public interface IDealFileAuthenticationRequestEditView:IView 
-    {
-    }
-}
